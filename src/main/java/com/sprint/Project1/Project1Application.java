7963fc8248d2d8d9bdd5f444b0a65e6ef418c212
rahulkanna25 <r-rahul.kanna@capgemini.com>
2024-12-18 22:16:45 +0530

    Update Project1Application.java

--- src/main/java/com/sprint/Project1/Project1Application.java
+++ src/main/java/com/sprint/Project1/Project1Application.java
@@ -2,8 +2,11 @@ package com.sprint.Project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@SpringBootApplication(scanBasePackages="com.controller,com.service")
+@EnableJpaRepositories(basePackages = "com.dao")
+@EntityScan(basePackages = "com.model")
 public class Project1Application {
 
 	public static void main(String[] args) {
